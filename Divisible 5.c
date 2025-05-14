e50191e4ee7451fa4459f30c77fe7fc7abeb93e6
jyothideepti <jyothideepti123@gmail.com>
2025-05-14 10:03:20 +0530

    Create Divisible 5.c

---
+++ Divisible 5.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main() 
+{
+    int n, 
+    printf("enter the value of n");
+    scanf("℅d",&n);
+    if(n ℅ 3==0&& n ℅5==0)
+    printf("divisible by both");
+    else
+  printf ("not divisible by both");
+}
